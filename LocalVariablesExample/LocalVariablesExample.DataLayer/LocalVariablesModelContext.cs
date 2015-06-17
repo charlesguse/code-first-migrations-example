c795dca516215f607d28af751acf08d5f5f4ab5f
Charlie <charlesguse@gmail.com>
2015-06-16 23:14:13 -0400

    Initial commit

---
+++ LocalVariablesExample/LocalVariablesExample.DataLayer/LocalVariablesModelContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocalVariablesExample.Data;
+
+namespace LocalVariablesExample.DataLayer
+{
+    // Open the Package Manager Console
+    // Set default project to LocalVariablesExample.DataLayer
+    // The important commands I have been using are
+    // Enable-Migrations 
+    // Add-Migration <Migration Name>
+    // Update-Database
+    // 
+    // When going to production:
+    // Update-Database -Script -SourceMigration:<Migration Name>
+    // Use -SourceMigration:0 when wanting all of the database changes from the beginning
+    // Update-Database -Script -SourceMigration:0
+    // ReSharper disable once ClassWithVirtualMembersNeverInherited.Global
+    public class LocalVariablesModelContext : DbContext
+    {
+        public LocalVariablesModelContext()
+            : base("LocalVariables")
+        { }
+
+        virtual public DbSet<Post> Posts { get; set; }
+    }
+}
