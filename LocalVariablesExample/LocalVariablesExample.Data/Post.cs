a82457605121e028eb50ffe74ca36baf0de70d76
Charlie Guse <charlesguse@users.noreply.github.com>
2015-06-16 23:14:13 -0400

    Initial commit

---
+++ LocalVariablesExample/LocalVariablesExample.Data/Post.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocalVariablesExample.Data
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(4, ErrorMessage = "Too long")]
+        public string Body { get; set; }
+
+        public Post()
+        {
+        }
+    }
+}
