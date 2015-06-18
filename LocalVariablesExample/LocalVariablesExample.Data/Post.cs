23b6a9c3cc07ddc3263ad56e8ee792e4842ae08c
Charlie Guse <charlesguse@users.noreply.github.com>
2015-06-17 23:17:37 -0400

    Tweaked the comments some and removed an unused constructor that will be readded during the demo.

--- LocalVariablesExample/LocalVariablesExample.Data/Post.cs
+++ LocalVariablesExample/LocalVariablesExample.Data/Post.cs
@@ -7,9 +7,5 @@ namespace LocalVariablesExample.Data
         public int Id { get; set; }
         [Required, MaxLength(4, ErrorMessage = "Too long")]
         public string Body { get; set; }
-
-        public Post()
-        {
-        }
     }
 }
